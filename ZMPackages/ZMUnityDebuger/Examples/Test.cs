65ee337caa170191ef2a38c81914a8cc0b476d68
zm <zhumengxyedu@163.com>
2024-09-01 23:12:11 +0800

    日志系统1.0.0

---
+++ ZMPackages/ZMUnityDebuger/Examples/Test.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    
+    void Start()
+    {
+        //初始化日志系统
+        LogSystem.InitLogSystem();
+    }
+
+  
+}
